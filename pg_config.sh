f5b78148385f0054b91f90df8bfdf02520a936c8
Jeff Reiher <jreiher2003@yahoo.com>
2016-07-01 21:40:01 +0000

    Add provisions to vagrant

---
+++ pg_config.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+echo "****** Provisioning virtual machine... ******"
+sudo apt-get -qqy update 
+sudo apt-get -qqy upgrade
+sudo apt-get -qqy install git 
+
+echo "******************** git configs **********************"
+git config --global user.name "Jeff Reiher"
+git config --global user.email "jreiher2003@yahoo.com"
+git config --global push.default upstream 
+git config --global merge.conflictstyle diff3
+git config --global credential.helper 'cache --timeout=10000'
+
+wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+wget https://www.udacity.com/api/nodes/3333158951/supplemental_media/bash-profile-course/download
+
+cat download >> .bashrc
+rm download
+
+
+echo "*********** done ************************"
+
+
